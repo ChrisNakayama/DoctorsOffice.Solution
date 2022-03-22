8a212e008a173766b70c24d81204afbf33faa22a
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-03-21 20:44:27 -0700

    Added controllers
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- DoctorsOffice/Controllers/DoctorsController.cs
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -28,5 +28,39 @@ namespace DoctorsOffice.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+     public ActionResult Details(int id)
+    {
+      var thisDoctor = _db.Doctors
+      .Include(doctor => doctor.JoinEntities)
+      .ThenInclude(join => join.Patient)
+      .FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(thisDoctor);
+    }
+    public ActionResult Edit (int id)
+    {
+      var thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(thisDoctor);
+    }
+    [HttpPost]
+    public ActionResult Edit(Doctor doctor)
+    {
+      _db.Entry(doctor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete (int id)
+    {
+      var thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(thisDoctor);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      _db.Doctors.Remove(thisDoctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
